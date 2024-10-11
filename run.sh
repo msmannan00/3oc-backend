26ca0a9f071ed0ef5f0844c3c6c54013bdf45b8c
Syed Umair Ul Hassan <64380267+alphadeveloper12@users.noreply.github.com>
2024-10-11 15:21:04 +0500

    nginx configured

--- run.sh
+++ run.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
+docker-compose down
 
-# This script will build and run the Docker containers for the Django project
-
-# Step 1: Build the Docker images
 echo "Building Docker images..."
 docker-compose build
 
 # Step 2: Run the Docker containers
 echo "Starting Docker containers..."
 docker-compose up
-
-# Optional: To run migrations automatically after the containers are up
-# Uncomment the following line if you want to run migrations automatically
-# echo "Running migrations..."
-# docker-compose exec web python manage.py migrate
-
-# Optional: To run tests automatically after the containers are up
-# Uncomment the following line if you want to run tests automatically
-# echo "Running tests..."
-# docker-compose exec web python manage.py test
